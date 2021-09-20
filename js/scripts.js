8234ee9bc779996a6b7858ec3aa9971f0b54db12
i-diordiev <i.diordiev-2027@kpi.ua>
2021-09-20 17:25:15 +0300

    added lab1 text files, moved .css and .js to folders

---
+++ js/scripts.js
@@ -0,0 +1,81 @@
+var labPath = "";
+
+            function showDescription() {
+                var element = document.getElementById("stringfortext");
+                element.innerText = "DESCRIPTION";
+            }
+
+            function showTheme() {
+                var element = document.getElementById("stringfortext");
+                element.innerText = "THEME";
+            }
+
+            function showStructure() {
+                var element = document.getElementById("stringfortext");
+                element.innerText = "STRUCTURE";
+            }
+
+            function showHtmlTableCode() {
+                var element = document.getElementById("stringfortext");
+                element.innerText = "TABLE CODE";
+            }
+
+            function showHtmlFormCode() {
+                var element = document.getElementById("stringfortext");
+                element.innerText = "FORM CODE";
+            }
+
+            function showHtmlImageCode() {
+                var element = document.getElementById("stringfortext");
+                element.innerText = "IMAGE CODE";
+            }
+
+            function showConclusion() {
+                var element = document.getElementById("stringfortext");
+                element.innerText = "CONCLUSION";
+            }
+
+            function chooseLab1() {
+                labPath = "lab1/";
+                document.getElementById("startString").hidden = "true";
+            }
+
+            function chooseLab2() {
+                labPath = "lab2/";
+                document.getElementById("startString").hidden = "true";
+            }
+
+            function chooseLab3() {
+                labPath = "lab3/";
+                document.getElementById("startString").hidden = "true";
+            }
+
+            function chooseLab4() {
+                labPath = "lab4/";
+                document.getElementById("startString").hidden = "true";
+            }
+
+            function chooseLab5() {
+                labPath = "lab5/";
+                document.getElementById("startString").hidden = "true";
+            }
+
+            function chooseLab6() {
+                labPath = "lab6/";
+                document.getElementById("startString").hidden = "true";
+            }
+
+            function chooseLab7() {
+                labPath = "lab7/";
+                document.getElementById("startString").hidden = "true";
+            }
+
+            function chooseLab8() {
+                labPath = "lab8/";
+                document.getElementById("startString").hidden = "true";
+            }
+
+            function chooseLab9() {
+                labPath = "lab9/";
+                document.getElementById("startString").hidden = "true";
+            }
